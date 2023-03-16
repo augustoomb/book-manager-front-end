0473cfc057cd340e0173b3e3d222a4f73026e821
augusto barbosa <augustobarbosa1988@gmail.com>
2023-03-16 08:50:34 -0300

    feat: home mostra resultado da busca e esconde conteúdo original, caso a busca seja usada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './Home.module.css';
 import Header from '../components/Header';
 import MenuAside from '../components/MenuAside';
@@ -6,39 +7,42 @@ import Book from '../components/Book';
 import searchGoogleBooksApi from '../services/googleBooks';
 
 function Home() {
+  const [dataFound, setDataFound] = useState('');
+
   const handleSearch = async (searchInputValue) => {
     const data = await searchGoogleBooksApi(searchInputValue);
-    console.log(data);
+    setDataFound(data.items);
   };
 
   return (
     <section className={ styles.container }>
       <MenuAside />
       <Header onClick={ handleSearch } />
-      <main className={ styles.main }>
-        <HomeBanner />
-        <div className={ styles.recentlyAddedList }>
-          <h2>Adicionados recentemente à minha biblioteca</h2>
-          <div className={ styles.bookCollection }>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-          </div>
-        </div>
-      </main>
+      {
+        dataFound === '' ? (
+          <main className={ styles.mainBase }>
+            <HomeBanner />
+            <div className={ styles.recentlyAddedList }>
+              <h2>Adicionados recentemente à minha biblioteca</h2>
+              <div className={ styles.bookCollection }>
+                <Book />
+                <Book />
+                <Book />
+              </div>
+            </div>
+          </main>
+        ) : (
+          <main className={ styles.mainHide }>
+            {
+              dataFound.map((book, index) => (<Book
+                key={ index }
+                title={ book.volumeInfo.title || 'título indefinido' }
+                author={ book.volumeInfo.authors || 'autor indefinido' }
+              />))
+            }
+          </main>
+        )
+      }
     </section>
   );
 }
