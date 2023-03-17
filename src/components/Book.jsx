2663576760a9655066cf9104e21765e139ca5c17
augusto barbosa <augustobarbosa1988@gmail.com>
2023-03-17 15:04:45 -0300

    feat: criando efeito hover no componente book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/prop-types */
 import styles from './Book.module.css';
 // import BookCover from '../images/book-cover.png';
@@ -5,7 +6,16 @@ import styles from './Book.module.css';
 function Book({ title, author, image }) {
   return (
     <div className={ styles.bookArea }>
-      <img src={ image } alt="book-cover" className={ styles.bookImg } />
+      <div className={ styles.container }>
+        <img
+          src={ image }
+          alt="book-cover"
+          className={ styles.image }
+        />
+        <div className={ styles.middle }>
+          <div className={ styles.text }>John Doe</div>
+        </div>
+      </div>
       <div className={ styles.bookInfo }>
         <p className={ styles.bookInfoTitle }>{title}</p>
         <p className={ styles.bookInfoAuthor }>{author}</p>
