46b2b5c68fd3d8d79abe1a3d1be3ac44805b72fd
augusto barbosa <augustobarbosa1988@gmail.com>
2023-03-19 21:24:59 -0300

    fix: removendo slice de biblioteca.

---
+++ src/redux/slices/bookLib.jsx
@@ -0,0 +1,26 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState = {
+//   myLib: [],
+// };
+
+// export const bookLibSlice = createSlice({
+//   name: 'bookLib',
+//   initialState,
+//   reducers: {
+//     addBook: (state, action) => {
+//       state.myLib.push(action.payload.id);
+//     },
+//     // removeToken: (state) => {
+//     //   state.token = '';
+//     // },
+//     // changeUser: (state, action) => {
+//     //   state.name = action.payload.name;
+//     //   state.email = action.payload.email;
+//     //   state.role = action.payload.role;
+//     // },
+//   },
+// });
+
+// export const { addBook } = bookLibSlice.actions;
+// export default bookLibSlice.reducer;
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import user from './slices/user';
+// import bookLib from './slices/bookLib';
 
 const store = configureStore({
   reducer: {
     user,
+    // bookLib,
   },
 });
 
