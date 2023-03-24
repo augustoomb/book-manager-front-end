e8d4acb867691f6f190df984449d05b7767c438f
augusto barbosa <augustobarbosa1988@gmail.com>
2023-03-24 15:05:09 -0300

    fix: removendo try/catch

--- src/services/book.jsx
+++ src/services/book.jsx
@@ -3,41 +3,46 @@ import axios from 'axios';
 const backUrl = process.env.REACT_APP_BACKEND_URL;
 
 const saveBook = async (objBook) => {
-  try {
-    const response = await axios.post(`${backUrl}/books`, {
+  const response = await axios.post(`${backUrl}/books`, {
+    title: objBook.title,
+    thumb: objBook.sendImg,
+    hasBeenRead: objBook.hasBeenRead,
+    authorName: objBook.author,
+    infoLink: objBook.infoLink,
+  }, {
+    headers: {
+      Authorization: localStorage.getItem('tokenLogin'),
+    },
+  });
+  return response.data;
+};
+
+const getAllBooksByUser = async () => {
+  const response = await axios.get(`${backUrl}/books`, {
+    headers: {
+      Authorization: localStorage.getItem('tokenLogin'),
+    },
+  });
+  return response.data;
+};
+
+const updateBook = async (id, objBook) => {
+  const response = await axios.put(
+    `${backUrl}/books/${id}`,
+    {
       title: objBook.title,
       thumb: objBook.sendImg,
       hasBeenRead: objBook.hasBeenRead,
       authorName: objBook.author,
       infoLink: objBook.infoLink,
-    }, {
+    },
+    {
       headers: {
         Authorization: localStorage.getItem('tokenLogin'),
       },
-    });
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return {
-      error: error.response.data.message,
-    };
-  }
+    },
+  );
+  return response.status;
 };
 
-const getAllBooksByUser = async () => {
-  try {
-    const response = await axios.get(`${backUrl}/books`, {
-      headers: {
-        Authorization: localStorage.getItem('tokenLogin'),
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return {
-      error: error.response.data.message,
-    };
-  }
-};
-
-export { saveBook, getAllBooksByUser };
+export { saveBook, getAllBooksByUser, updateBook };
